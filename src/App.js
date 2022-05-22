15f70a40fb8b4e5d3b29b8f24816ce416126ca26
Ayah <eng.aya.elsayed.2020@gmail.com>
2022-05-22 11:40:32 +0200

    apply simple route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,34 @@
 import Products from './components/Products';
-import './style.scss'
+import './style.scss';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div >
-      <Products />
+
+      <Routes>
+        <Route path="/" element={<Products />} />
+        <Route path="cart" element={<Cart />} />
+      </Routes>
+
+      {/* <Products /> */}
     </div>
   );
 }
 
+function Cart() {
+  return (
+    <>
+      <main>
+        <h2>Who are we?</h2>
+        <p>
+          Go to all products
+        </p>
+      </main>
+      <nav>
+        <Link to="/"> products</Link>
+      </nav>
+    </>
+  );
+}
 export default App;
--- src/components/Products.js
+++ src/components/Products.js
@@ -3,16 +3,21 @@ import { Container, Row, Col } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import products from '../data'
 import Product from './product/Product'
+import { Link } from "react-router-dom";
 
 const Products = () => {
 
   return (
     <Container fluid>
+      <div className='title'>
+        Welcome to our Ammazing shop
+        <Link to="/cart">Cart</Link>
+      </div>
       <Row className='justify-content-center'>
         {products.map((product) => (
           <Col sm={12} md={5} lg={3} className="  m-2" key={product.id}>
             {/* {product.name} */}
-            <Product  product={product}/>
+            <Product product={product} />
           </Col>
         ))}
       </Row>
